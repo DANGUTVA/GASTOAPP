044519fbe5a65a2cfd25a637292342bb8eb25687
Danny Gutierrez <danygv@gmail.com>
2025-02-10 19:42:53 -0600

    eliminar check

--- src/components/expense/CostCenterSelect.tsx
+++ src/components/expense/CostCenterSelect.tsx
@@ -13,31 +13,42 @@ interface CostCenterSelectProps {
   costCenter: string;
   costCenters: string[];
   onValueChange: (value: string) => void;
+  onAddNewCostCenter?: () => void;
+  className?: string;
 }
 
-export const CostCenterSelect = ({ costCenter, costCenters, onValueChange }: CostCenterSelectProps) => {
+export const CostCenterSelect = ({ costCenter, costCenters, onValueChange, onAddNewCostCenter, className }: CostCenterSelectProps) => {
+  const handleValueChange = (value: string) => {
+    if (value === 'new') {
+      if (onAddNewCostCenter) {
+        onAddNewCostCenter();
+      }
+    } else {
+      onValueChange(value);
+    }
+  };
+
   return (
-    <Select 
-      value={costCenter} 
-      onValueChange={onValueChange}
-    >
-      <SelectTrigger>
-        <SelectValue placeholder="Seleccione un centro de costo" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {costCenters.map((center) => (
-            <SelectItem key={center} value={center}>
-              {center}
+    <div className={className}>
+      <Select value={costCenter} onValueChange={handleValueChange}>
+        <SelectTrigger>
+          <SelectValue placeholder="Seleccione un centro de costo" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {costCenters.map((center) => (
+              <SelectItem key={center} value={center}>
+                {center}
+              </SelectItem>
+            ))}
+            <SelectSeparator />
+            <SelectItem value="new" className="text-blue-600">
+              <Plus className="w-4 h-4 mr-2 inline-block" />
+              Agregar nuevo centro de costo
             </SelectItem>
-          ))}
-          <SelectSeparator />
-          <SelectItem value="new" className="text-blue-600">
-            <Plus className="w-4 h-4 mr-2 inline-block" />
-            Agregar nuevo centro de costo
-          </SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   );
 };
